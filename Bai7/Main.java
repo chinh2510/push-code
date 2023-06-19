fa4622cbc2da6337341b429e4369f039c61caded
chinh2510 <129308513+chinh2510@users.noreply.github.com>
2023-06-19 17:34:37 +0700

    Add files via upload

---
+++ Bai7/Main.java
@@ -0,0 +1,11 @@
+package Bai7Hinhtron;
+
+public class Main {
+	public static void main(String[] args) {
+		ToaDo toaDo = new ToaDo(2f, 5F);
+		HinhTron ht = new HinhTron(toaDo, 10.5);
+		
+		System.out.println(ht);
+
+	}
+}
